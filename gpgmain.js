764893285c7bdfe1a725a613d7be9653e86ea5aa
fredp1edu <frederick.pierce@mail.citytech.cuny.edu>
2017-02-27 19:06:54 -0500

    fred commit 1

---
+++ gpgmain.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+/* app.use(function(request, response, next) {
+    response.locals.showTests = app.get('env') !== 'production' && request.query.test === '1';
+    next();
+}); */
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+   response.render('home');
+});
+
+app.use(function(request, response) {
+    response.status(404);
+    response.render('404', {layout: 'error'});
+});
+app.use(function(err, request, response, next) {
+    console.error(err.stack); response.type('text/plain');
+    response.status(500);
+    response.render('500');
+});
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Cntrl-C to terminate.');
+});
