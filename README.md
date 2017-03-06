a06566c1b7720812007caa3e38fcc4d005b26b3f
Nicholas Galarza <nicogala93@gmail.com>
2017-03-05 21:53:19 -0500

    Added reCApTCHA

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # GenderPayGap is a tool used to compare salaries of different genders with the same level of expertise and experience.
 
-If ever at all you need to take everything down and reset your project by placing it into a new directory. Re-install the packages by doing: npm install express, npm install express3-handlebars, npm install body-parser, and then you will be able to run the project. 
+If ever at all you need to take everything down and reset your project by placing it into a new directory. Re-install the packages by doing:
+1.) npm install express
+2.) npm install express3-handlebars
+3.) npm install body-parser
--- gpgmain.js
+++ gpgmain.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
-var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+var handlebars = require('express3-handlebars').create({
+    defaultLayout: 'main'
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 var params = require('./lib/gpgParams.js');
 
 app.set('port', process.env.PORT || 3000);
-/* 
+/*
     app.use(require('cookie-parser')(credentials.cookieSecret));
     app.use(require('express-session')());
     app.use(function(request, response, next) {
@@ -19,14 +21,49 @@ app.use(require('body-parser')());
 var inpEmail, inpForm;
 
 app.get('/', function(request, response) {
-   response.render('home');
+    response.render('home');
 });
 app.get('/confirm', function(request, response) {
-    response.render('confirm', {inpEmail: inpEmail, 
-                                params: params });
+    response.render('confirm', {
+        inpEmail: inpEmail,
+        params: params
+    });
 });
+
+app.post('/confirm', function(request, response) {
+    // Recaptcha-Response
+    if (request.body['g-recaptcha-response'] === undefined ||
+        request.body['g-recaptcha-response'] === '' ||
+        request.body['g-recaptcha-response'] === null) {
+        return response.json({
+            "responseCode": 1,
+            "responseDesc": "Please select captca"
+        });
+    }
+    // Insert Secret Key.
+    var secretKey = "6Le8sBcUAAAAANLR9rp8WhGVHjXApztLLmsbkK-_";
+    var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + request.body['g-recaptcha-response'] + "&remoteip=" + request.connection.remoteAddress;
+    // Hitting GET request to the URL, Google responds with success or error.
+    request(verificationUrl, function(error, response, body) {
+        body = JSON.parse(body);
+        // Success will be true or false depending on validation.
+        if (body.success !== undefined && !body.success) {
+            return response.json({
+                "responseCode": 1,
+                "responseDesc": "Failed captcha verification"
+            });
+        }
+    });
+    response.json({
+        "responseCode": 0,
+        "responseDesc": "Success"
+    });
+});
+
 app.get('/thanks', function(request, response) {
-    response.render('thanks', {inpEmail: inpEmail });
+    response.render('thanks', {
+        inpEmail: inpEmail
+    });
 });
 app.post('/', function(request, response) {
     inpEmail = request.body.inEmail;
@@ -35,13 +72,17 @@ app.post('/', function(request, response) {
 });
 app.use(function(request, response) {
     response.status(404);
-    response.render('404', {layout: 'error'});
+    response.render('404', {
+        layout: 'error'
+    });
 });
 app.use(function(err, request, response, next) {
     console.error(err.stack);
     response.status(500);
-    response.render('500', {layout: 'error'});
+    response.render('500', {
+        layout: 'error'
+    });
 });
 app.listen(app.get('port'), function() {
-    console.log('Express started on http://localhost:' + app.get('port') + '; press Cntrl-C to terminate.');
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
