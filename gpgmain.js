dc1d5146b6620eab078717ce71186b1ba875ba2a
fredp1edu <frederick.pierce@mail.citytech.cuny.edu>
2017-03-11 13:23:39 -0500

    Fred commit 4

--- gpgmain.js
+++ gpgmain.js
@@ -7,37 +7,103 @@ app.use(require('body-parser')());
 var credentials = require('./credentials.js');      // remember to set your credentials.js file 
 app.use(require('cookie-parser')(credentials.cookieSecret));
 app.use(require('express-session')());
-var params = require('./lib/gpgParams.js');
 
-/*  the two lines are for mocha/chai testing
-    app.use(function(request, response, next) {
-    response.locals.showTests = app.get('env') !== 'production' && request.query.test === '1';
-    next();
-}); */
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+var url = credentials.mongoUrl;
+
+var params = require('./lib/gpgParams.js');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
+// page display and get/post functions 
 app.get('/', function(request, response) {
-   response.render('home', {pgTitle: params.getPgTitle('home') });
+   response.render('home', {pgTitle: params.getPgTitle('home'),
+                            addCaptcha: true });
 });
 app.post('/', function(request, response) {
-    var inpEmail = request.body.inpEmail;                       // add server-side verification
-    // BILLY! take inpEmail test to see if in db, if not, create new record with email, get id number
-    // redirect to different page if email is in db already
-    // var dbENum = [pending email collection id num];                                                
-    // request.session.dbENum = dbENum; 
+    // Nicholas captcha code 
+    if (request.body['g-recaptcha-response'] === undefined ||
+        request.body['g-recaptcha-response'] === '' ||
+        request.body['g-recaptcha-response'] === null) {
+        return response.json({
+            "responseCode": 1,
+            "responseDesc": "Please select captca"
+        });
+    }
+    // Insert Secret Key.
+    var secretKey = credentials.secretKey;
+    var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + 
+        request.body['g-recaptcha-response'] + "&remoteip=" + request.connection.remoteAddress;
+    // Hitting GET request to the URL, Google responds with success or error.
+    request(verificationUrl, function(error, response, body) { 
+        body = JSON.parse(body);
+        // Success will be true or false depending on validation.
+        if (body.success !== undefined && !body.success) {
+            return response.json({
+                "responseCode": 1,
+                "responseDesc": "Failed captcha verification"
+            });
+        }
+    });
+    response.json({
+        "responseCode": 0,
+        "responseDesc": "Success"
+    });
+    //end Nicholas captcha code
+
+    var inpEmail = request.body.inpEmail;                       // FRED - add server-side email verification
+    
+    /* BILLY - take inpEmail, do a find, if doesn't exist, create new record with email, then find email, get id number
+        if email is found, redirect to different page
+            if (email already in db) {
+                response.redirect(303, '/returnuser');
+            }  
+        we can then deal with existing emails later (i haven't created the /email check page yet so don't add to code yet). 
+        var dbEnum = [id of email];
+        request.session.dbENum = dbENum; 
+    */
+    var insertDocuments = function(db, callback){
+		//Get the documents collection
+		var collection = db.collection('documents');
+		//Insert some documents
+		collection.insert( {email: inpEmail, confirmed: false}, function(err, result) { // creates new record
+            collection.find({email: inpEmail}).toArray(function(err, info) {          // finds record just created, sends to array info
+                console.log(info[0]);                        // you can see here that it outputs the id but try getting it!
+                assert.equal(err, null);
+                assert.equal(1, result.result.n);
+                assert.equal(1, result.ops.length);
+                callback(result); 
+            });
+		});
+	};
+	// Use connect method to connect to the server
+	MongoClient.connect(url, function(err, db){
+		assert.equal(null, err);
+		console.log("Connected successfully to the server");
+		insertDocuments(db, function(){
+			//var collection = db.collection('documents');
+			//collection.remove();
+            db.close();
+		}); 
+	});
+    // End Billy mongo code
     request.session.inpEmail = inpEmail;                       
     response.redirect(303, '/thanks');
 });
 app.get('/thanks', function(request, response) {
     response.render('thanks', {pgTitle: params.getPgTitle('thanks'), 
                                inpEmail: request.session.inpEmail          //, dbENum: request.sesssion.dbENum
-                                }); 
+    }); 
+});
+app.get('/returnuser', function(request, response) {
+    response.render('returnuser', {pgTitle: params.getPgTitle('returnuser'), 
+                               inpEmail: request.session.inpEmail          //, dbENum: request.sesssion.dbENum
+    }); 
 });
 app.get('/confirm', function(request, response) {
     inpEmail = request.query.inpEmail;                // BILLY! take inpEmail, check against db num and email exist.
-                                                      //  if db can't confirm number, redirect to redirect page    
-    request.session.conEmail = inpEmail;              //  this will change to db generated email address once confirmed
+                                                      //  FRED - if db can't confirm number, redirect to redirect page    
+    request.session.conEmail = inpEmail;              //  this variable will change to db generated email address once confirmed
     response.render('confirm', {pgTitle: params.getPgTitle('confirm') });
     // how to do a time-delayed redirect to the dosurvey page
 });
@@ -80,6 +146,7 @@ app.get('/shosurvey', function(request, response) {      // add referrer page ve
                                   resDisp: resDisp });
 });
 
+//error page functions
 app.use(function(request, response) {                   // this for page not found error
     response.status(404);
     response.render('404', {layout: 'error'});
