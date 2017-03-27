9f29f2899768eabb0659c15ac2620dc5a5a59867
fredp1edu <frederick.pierce@mail.citytech.cuny.edu>
2017-03-27 15:22:38 -0400

    before 3/27 pull

--- gpgmain.js
+++ gpgmain.js
@@ -1,5 +1,4 @@
-/* Fred - take email submit and captcha out of home.handlebars and make a it partial to be inserted in home and email resubmit page,
-    also create an email resubmit page (for people who don't confirm email right away but email is in our db), set up and error page*/
+/* Fred - create an email resubmit page (for people who don't confirm email right away but email is in our db), set up and error page*/
 
 var express = require('express');
 var app = express();
@@ -10,7 +9,6 @@ app.use(require('body-parser')());
 var credentials = require('./credentials.js');      // remember to set your credentials.js file 
 app.use(require('cookie-parser')(credentials.cookieSecret));
 app.use(require('express-session')());
-                                                    
 var mongoose = require('mongoose');             // mongoose connection and schema builders
 mongoose.Promise = global.Promise;
 var opts = {
@@ -28,78 +26,94 @@ switch(app.get('env')) {
     default:
         throw new Error('Unknown execution environment: ' + app.get('env'));
 };
-var Unconfirmed = require('./models/unconfirmed.js');
-var Confirmed = require('./models/confirmed.js');
+var Unconfirmed = require('./models/unconfirmed.js');            // mongoose schema
+var Confirmed = require('./models/confirmed.js');                // mongoose schema
                 
 var params = require('./lib/gpgParams.js');                 // the main parameters for the site
+var CaptchaChek = require('./lib/gpgCaptcha.js');           // captcha verification moved here.
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
-
+app.use(function(request, response, next) {                 // for flash error messages
+    response.locals.flash = request.session.flash;
+    delete request.session.flash;
+    next();
+});
 // page display and get/post functions 
 app.get('/', function(request, response) {
-   response.render('home', {pgTitle: params.getPgTitle('home'),
+   response.render('home', {pgTitle: params.getPgTitle('home'),   
+                            inpEmail: (request.session.inpEmail) ? request.session.inpEmail : false,
+                            errorMsg: (request.session.errorMsg) ? request.session.errorMsg : false,
                             addCaptcha: true });
 });
 app.post('/', function(request, response) {
-    // captcha code 
-    if (request.body['g-recaptcha-response'] === undefined ||
-        request.body['g-recaptcha-response'] === '' ||
-        request.body['g-recaptcha-response'] === null) {
-        return response.json({
-            "responseCode": 1,
-            "responseDesc": "Please select captca"
-        });
-    }
-    // Insert Secret Key.
-    var secretKey = credentials.secretKey;
-    var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + 
-        request.body['g-recaptcha-response'] + "&remoteip=" + request.connection.remoteAddress;
-    // Hitting GET request to the URL, Google responds with success or error.
-    request(verificationUrl, function(error, response, body) { 
-        body = JSON.parse(body);
-        // Success will be true or false depending on validation.
-        if (body.success !== undefined && !body.success) {
-            return response.json({
-                "responseCode": 1,
-                "responseDesc": "Failed captcha verification"
-            });
-        }
-    });
-    response.json({
-        "responseCode": 0,
-        "responseDesc": "Success"
-    });
-    //end captcha code
-
     var inpEmail = request.body.inpEmail.trim();                    // FRED - add server-side email verification
     if (inpEmail === "") return response.redirect(303, '/');
     request.session.inpEmail = inpEmail;                            // add email input to session memory
     
-    Unconfirmed.findOne({userEmail: inpEmail}, '_id', function(err, user) {
-        if (user) {                                                 // if email exists already, do not re-save
-            if (!user.confirmed)                                    // if email exists but is not confirmed
-                return response.redirect(303, '/returnuser');       // redirect to resubmit page (once it's created)
-            else 
-                return response.redirect(303, '/returnuser');      // if email exists and confirmed, redirect to returnuser
-        }
-        new Unconfirmed({                                         // add email to unconfirmedemails collec. 
-            userEmail: inpEmail                 // instead of making another db request in '/thanks' get id here
-        }).save();
-        response.redirect(303, '/thanks');
-    });
+    var dbSave = function() {                                       // this is a callback after the captchachek
+        Unconfirmed.findOne({userEmail: inpEmail}, '_id', function(err, user) {
+            if (err) throw err;
+            if (user) {                                                 // if email exists already, do not re-save
+                console.log(user);
+                if (!user.confirmed)                                    // if email exists but is not confirmed
+                    return response.redirect(303, '/resubmit');       // redirect to FRED!! -- create resubmit page 
+                else 
+                    return response.redirect(303, '/returnuser');      // if email exists and confirmed, redirect to returnuser
+            }
+            new Unconfirmed({                                         // add email to unconfirmedemails collec. 
+                userEmail: inpEmail                
+                }).save();
+            if (request.session.errorMsg) delete request.session.errorMsg;
+            response.redirect(303, '/thanks');
+        });
+    };
+    CaptchaChek(request, response, credentials.secretKey, '/', dbSave);   //captchacheck verification
+});
+app.get('/resubmit', function(request, response) {
+    var inpEmail;
+    if (request.session.inpEmail) {
+        inpEmail = request.session.inpEmail;
+    } else {
+        request.session.flash = {
+            type: 'warning',
+            intro: 'Internal Error',
+            message: 'An error occured. Please start over.',
+        };
+        return response.redirect(303, '/');
+    }
+    response.render('resubmit', {pgTitle: params.getPgTitle('resubmit'), 
+                                   inpEmail: inpEmail,                                        
+    }); 
+    
 });
 app.get('/thanks', function(request, response) {
-    if (!request.session.inpEmail) 
+    var inpEmail;
+    if (request.session.inpEmail) {
+        inpEmail = request.session.inpEmail;
+    } else {
+        request.session.flash = {
+            type: 'warning',
+            intro: 'Internal Error',
+            message: 'An error occured. Please start over.',
+        };
         return response.redirect(303, '/');
-    Unconfirmed.findOne({userEmail: request.session.inpEmail}, '_id', function(err, user) {         //get id of email input
-        if (!user) {
-            return response.redirect(303, '/');
-        }
+    }
+    var mailAndRender = function(idNum) {
         response.render('thanks', {pgTitle: params.getPgTitle('thanks'), 
-                                   inpEmail: request.session.inpEmail, 
-                                   dbENum: user._id                                                 // send id to 'email' link
+                                   inpEmail: inpEmail, 
+                                   dbENum: idNum,                                                // send id to 'email' link
         }); 
-    });
+    };
+    if (request.session.dbENum) {
+        mailAndRender(request.session.dbENum);
+    } else {
+        Unconfirmed.findOne({userEmail: request.session.inpEmail}, '_id', function(err, user) {         //get id of email input
+            if (!user) {
+                return response.redirect(303, '/');
+            }
+            mailAndRender(user._id);
+        });
+    }
 });
 app.get('/confirm', function(request, response) {   // Fred - add referrer page restriction to disable back button
     var unconfE;
@@ -164,7 +178,7 @@ app.post('/prosurvey', function(request, response) {        //this function proc
     
     var passThru = true;                                   
     var inpForm = {
-        surveyDate: new Date()                // this isn't going through, check -FRED
+        surveyDate: new Date                // this isn't going through, check -FRED
     };
     for (var i = 0; i < params.getReqFieldLen(); i++) {
         inpForm[params.getReqField(i)] = request.body[params.getReqField(i)];
@@ -223,19 +237,6 @@ app.use(function(err, request, response, next) {         // this for program/db 
 app.listen(app.get('port'), function() {
     console.log('Express started on http://localhost:' + app.get('port') + '; press Cntrl-C to terminate.');
 });
-
-    /* TO DO 
-    1. create display page for all the survey entries in collection
-    2. finish making the survey entries (a few radio boxes, a comment box)
-    2a Add session fields to the handlebar documents (part of server side validation)
-    3. set up front and back end validation of all inputs - including error msgs for missing inputs
-    4. set up filtering of all inputs to prevent database hacking code 
-    5. create error pages and redirect pages if emails are unconfirmed or email is confirmed but survey never taken
-    6 clean up css - pretty things up
-    7. THE BIG GOALS:  setup member login page (so an existing member can edit or add another job survey data)
-    8. THE BIG GOALS:  set up different queries so user can query by state, gender, age, etc.
-    9. TEAM GOALs: finish the captcha code, 
-    10. TEAM GOALS:  get the APIs from other sites up and working */
 
     
     
