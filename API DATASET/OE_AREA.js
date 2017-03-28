121534a1a3840c27c33ee5e0d20c07f6b86b960a
billywu94 <billy.wu02@myhunter.cuny.edu>
2017-03-28 17:41:25 -0400

    Resolved Typeerror: request path contains unescaped character

--- API DATASET/OE_AREA.js
+++ API DATASET/OE_AREA.js
@@ -1,13 +1,14 @@
-var realmStatus = "http://api.dol.gov/V1/Statistics/OES/OE_AREA/?KEY=1ce7650d-b131-4fb7-91b3-b7761efc8cd4/&$select=AREA_CODEeq'0040380'";
-
+var realmStatus = "http://api.dol.gov/V1/Statistics/OES/OE_AREA/?KEY=1ce7650d-b131-4fb7-91b3-b7761efc8cd4&$filter=AREA_CODE eq'0040380'";
+var encode = encodeURI(realmStatus);
     //"http://api.dol.gov/V1/WHPS/?KEY=1ce7650d-b131-4fb7-91b3-b7761efc8cd4";
     //source: http://stackoverflow.com/questions/17811827/get-a-json-via-http-request-in-nodejs
+    //TypeError: Request path contains unescaped characters --> solution: https://www.w3schools.com/jsref/jsref_encodeuri.asp
 
 var http = require("http");
 
 var options = {
         host: 'api.dol.gov',
-        path: realmStatus,
+        path: encode,
         type: 'GET',
         dataType: 'json',
         headers: {'accept' : 'application/json'}
