2fe00ce18b5671dc3554b604e1c91309dc6e28b3
billywu94 <billy.wu02@myhunter.cuny.edu>
2017-04-03 12:14:07 -0400

    minor additions for testing data as a whole

--- API-DATASET/OE_DATA_PUB.js
+++ API-DATASET/OE_DATA_PUB.js
@@ -1,4 +1,6 @@
-var realmStatus = "http://api.dol.gov/V1/Statistics/OES/OE_DATA_PUB/?KEY=1ce7650d-b131-4fb7-91b3-b7761efc8cd4";
+var realmStatus = "http://api.dol.gov/V1/Statistics/OES/OE_DATA_PUB/?KEY=1ce7650d-b131-4fb7-91b3-b7761efc8cd4&$filter=SERIES_ID eq 'OEUS360000000000015113102'";
+var encode = encodeURI(realmStatus);
+
 
     //"http://api.dol.gov/V1/WHPS/?KEY=1ce7650d-b131-4fb7-91b3-b7761efc8cd4";
     //source: http://stackoverflow.com/questions/17811827/get-a-json-via-http-request-in-nodejs
@@ -7,7 +9,7 @@ var http = require("http");
 
 var options = {
         host: 'api.dol.gov',
-        path: realmStatus,
+        path: encode,
         type: 'GET',
         dataType: 'json',
         headers: {'accept' : 'application/json'}
